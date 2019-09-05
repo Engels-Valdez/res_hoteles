81edcfe5c3302c5d6bac2b0d9b7799f214ee8000
--replace <e_valdezcastillo@outlook.com>
2019-09-05 13:09:59 -0400

    Se agregaron los archivos de boostrap y jquery, y se instalaron en la primera vista creada para probar todo:hotelView

---
+++ rt/hotel.php
@@ -0,0 +1,1 @@
+<?php require_once('../view/page/HotelView.php'); ?>
---
+++ view/page/HotelView.php
@@ -0,0 +1,25 @@
+<?php require_once('../view/page/layout/head.php') ?>
+    <title>Hoteles</title>
+</head>
+<body>
+<header>
+    <?php require_once('../view/page/layout/nav.php') ?>
+</header>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <main>
+                
+            </main>
+        </div>
+    </div>
+</div>
+
+<?php require_once('../view/page/layout/footer.php') ?>
+
+<script src="../view/js/jquery-3.4.1.min.js"></script>
+<script src="../view/js/main.js"></script>
+<script src="../view/js/bootstrap.min.js"></script>
+</body>
+</html>
