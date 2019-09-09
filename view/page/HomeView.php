205b64b0883dea7e9af11630025367123200df91
--replace <e_valdezcastillo@outlook.com>
2019-09-09 12:31:01 -0400

    Se agrego el mapa de diseño al home

--- view/page/HomeView.php
+++ view/page/HomeView.php
@@ -318,6 +318,16 @@
                 </section>
             </main>
         </div>
+        <div class="col-md-2">
+            <aside>
+                <div class="card" style="width: 18rem;">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241953.25400494854!2d-68.61143202233569!3d18.640665633413388!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ea891645dcbfe77%3A0x61881cfaed12f6f3!2sPunta%20Cana!5e0!3m2!1ses-419!2sdo!4v1568046050297!5m2!1ses-419!2sdo" width="286" height="350" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                <div class="card-body">
+                    <center><p class="card-text">Localizacion</p></center> 
+                </div>
+                </div>
+            </aside>
+        </div>
     </div>
 </div>    
 <?php require_once('../view/page/layout/footer.php') ?>
