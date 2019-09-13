4edf8374792ab7584da86a04bc318ba026311cbd
--replace <e_valdezcastillo@outlook.com>
2019-09-13 01:35:34 -0400

    Se agrego la clase de conexion

--- model/Conexion.php
+++ model/Conexion.php
@@ -2,4 +2,19 @@
 
     class Conexion{
         
+        public static function conectar(){
+
+            try {
+
+                $conec = new PDO('mysql:host=localhot; dbname=res_hoteles','root','pHe220728?');
+
+            } catch (PDOException $e) {
+
+                echo 'Error: '.$e->getMessage();
+                
+            }
+
+            return $conec;
+        }
+
     }
