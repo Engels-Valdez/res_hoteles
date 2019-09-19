675e796cf6c6e34cb5747c27003b71e2a20ca358
--replace <e_valdezcastillo@outlook.com>
2019-09-19 15:46:20 -0400

    Se creo una pagina de error

---
+++ view/page/ErrorView.php
@@ -0,0 +1,23 @@
+<?php require_once('../view/page/layout/head.php') ?>
+    <title>Reservaciones</title>
+</head>
+<body class="bg-light">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <center>
+                <h1 class="display-1 mt-5">404</h1>
+                <p>Registro no encontrado o no esta disponible.</p>
+                <a href="../rt/home.php">Ir al inicio</a>
+                </center>
+            </div>
+        </div>
+    </div>
+
+<script src="../view/js/jquery-3.4.1.min.js"></script>
+<script src="../view/js/main.js"></script>
+<script src="../view/js/bootstrap.min.js"></script>
+<script src="../view/js/form-validation.js"></script>
+</body>
+</html>
