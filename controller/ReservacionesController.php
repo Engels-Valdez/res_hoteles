5d90fa248d7a05bd976cd0d2a9d7356251380ea8
--replace <e_valdezcastillo@outlook.com>
2019-09-20 12:07:34 -0400

    Se modifico el formulario y ya se genera el total de precio por noches y hace el calculo de candidad de noche entre dos fechas

--- controller/ReservacionesController.php
+++ controller/ReservacionesController.php
@@ -1,5 +1,28 @@
 <?php
     require_once('../model/ReservacionesModel.php');
+    date_default_timezone_set('UTC');
+
+    $id_habitacion = $_GET['id'];
+    $entrada = $_GET['entrada'];
+    $salida = $_GET['salida'];
+    $en = new DateTime($entrada);
+    $sa = new DateTime($salida);
+    $noches = $en->diff($sa);
+
+
+
+    if( $_SERVER['REQUEST_METHOD'] == 'GET' && !empty( $id_habitacion ) ){
+
+        $resv = new ReservacionesModel();
+        $idPrecio = $resv->getIdPrecioHabitaciones($id_habitacion);
+        
+        if( empty($idPrecio) ){
+
+            header('Location: error.php');
+
+        }
+      
+    }
 
     require_once('../view/page/ReservacionesView.php');
 ?>
