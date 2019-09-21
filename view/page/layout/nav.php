8cd169600973cf91ef78625b51d67d050307942c
--replace <e_valdezcastillo@outlook.com>
2019-09-21 14:37:28 -0400

    Se configuro lo del inicio de seccion en la barra de navegacion

--- view/page/layout/nav.php
+++ view/page/layout/nav.php
@@ -1,9 +1,27 @@
+<?php session_start(); ?>
 <!-- Image and text -->
-<nav class="navbar navbar-dark bg-dark fixed-top">
-  <a class="navbar-brand" href="#">
-    <img src="../view/asset/mery.png" width="150" height="40" class="d-inline-block align-top" alt="">
-  </a>
-  <ul class="navbar-nav">
-    <li class="nav-items"><a href="#" class="nav-link">Registrar / Iniciar seccion</a></li>
-  </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+<img src="../view/asset/mery.png" width="150" height="40" class="d-inline-block align-top" alt="">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="col-md-5 offset-10">
+    <ul class="navbar-nav mr-auto">
+    <?php if( isset( $_SESSION['Seccion'] ) ) : ?>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <?= $_SESSION['Seccion'] ?>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="cerrar.php">Cerrar seccion</a>
+        </div>
+      </li>
+      <?php else : ?>
+      <li><a href="login.php">Iniciar seccion</a></li>
+      <?php endif;?>
+    </ul>
+    </div>
+  </div>
 </nav>
+
