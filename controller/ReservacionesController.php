9f134db3e80bd6b30987fe9e8837ff0a48dbd48e
--replace <e_valdezcastillo@outlook.com>
2019-09-21 15:18:54 -0400

    Se limito a que solo con una seccion iniciada se pueda acceder a esta pagina

--- controller/ReservacionesController.php
+++ controller/ReservacionesController.php
@@ -1,6 +1,12 @@
-<?php
+<?php session_start();
     require_once('../model/ReservacionesModel.php');
     date_default_timezone_set('UTC');
+
+    if( !isset( $_SESSION['Seccion'] ) ){
+
+        header('Location: login.php');
+
+    }
 
     $id_habitacion = $_GET['id'];
     $entrada = $_GET['entrada'];
