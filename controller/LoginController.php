e31bdf247169d6ba2c653c2f5cab905883c0417c
--replace <e_valdezcastillo@outlook.com>
2019-09-23 14:38:51 -0400

    Se programo el controlador para que cuando el usuario inicie se guarden las credenciales en la seccion

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -1,5 +1,30 @@
-<?php
+<?php session_start();
 require_once('../model/LoginModel.php');
+
+$correo = $_POST['correo'];
+$pass = $_POST['pass'];
+$frmInciarSeccion = $_POST['btnInciar'];
+
+if( $_SERVER['REQUEST_METHOD'] == 'POST' and !empty( $frmInciarSeccion ) ){
+
+    $access = new LoginModel();
+    $credenciales = $access->getCredenciales($correo,$pass);
+
+    if( !empty( $credenciales ) ){
+
+            $_SESSION['Seccion'] = $credenciales; //= $cdc['nombre']. ' ' .  $cdc['apellido'];
+            
+        header('Location: ../rt/home.php');
+
+    }else {
+
+        header('Location: ../index.php');
+
+    }
+
+
+}
+
 
 require_once('../view/page/LoginView.php');
 ?>
