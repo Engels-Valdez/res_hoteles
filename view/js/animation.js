a954ebf6289d80c08d86ad91a53c1bcad0180553
--replace <e_valdezcastillo@outlook.com>
2019-09-27 12:59:34 -0400

    Se creo el archivo para animaciones y se hicieron las primeras animaciones para el navbar, y otros componentes

---
+++ view/js/animation.js
@@ -0,0 +1,24 @@
+window.sr = ScrollReveal();
+sr.reveal('.jumbotron',{
+      duration: 2000,
+      origin: 'left',
+      distance: '300px'
+  });
+
+  sr.reveal('.post-destinos',{
+      duration: 2000,
+      origin: 'right',
+      distance: '300px'
+  });
+
+  sr.reveal('.navbar',{
+    duration: 2000,
+    origin: 'right'
+
+});
+
+sr.reveal('.articulos',{
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
