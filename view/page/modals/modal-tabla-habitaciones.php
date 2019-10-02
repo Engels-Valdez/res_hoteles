94888aa5707bcede002a3a29ed91ef472dffd242
--replace <e_valdezcastillo@outlook.com>
2019-10-02 11:56:42 -0400

    Se creo el modal para la table de habitaciones que pertenece al modulo de habitaciones

---
+++ view/page/modals/modal-tabla-habitaciones.php
@@ -0,0 +1,56 @@
+
+<style>
+table {
+  display: block;
+  height: 250px;
+  overflow-y: auto;
+}
+</style>
+
+<div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+        <div class="modal-body">
+        <div class="table-resposive mt-5">
+            <table class="table table-hover table-warning my-5">
+                <thead>
+                <tr>
+                    <th>Tipo</th>
+                    <th>Precio</th>
+                    <th>Precio de oferta</th>
+                    <th>Cantidad de Cama</th>
+                    <th>Cantidad de adultos</th>
+                    <th>Cantidad de niños</th>
+                    <th>Tamaño</th>
+                    <th>Detalle</th>
+                    <th>Diponible</th>
+                    <th>Hotel</th>
+                    <th>Foto</th>
+                    <th>Acciones</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                          <a href="?eliminar-habitacion = " class="btn btn=sm btn-danger btn-block">Eliminar</a>
+                          <a href="?editar-habitacion = " class="btn btn-sm- btn-success btn-block">Editar</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        </div>
+    </div>
+  </div>
+</div>
